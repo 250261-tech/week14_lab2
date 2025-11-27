ca2c358d972e95a6d0bd957fe6ec4677adc32282
Do'ngboyev Shaxriyor <dongboyevsh@gmail.com>
2025-11-27 12:24:43 +0500

    2nd

--- problem2.cpp
+++ problem2.cpp
@@ -3,5 +3,21 @@ using namespace std;
 
 
 int main() {
-
+    int arr[4] = {10,20,30,40};
+    int n;
+    string s;
+    cin>>n;
+    try {
+        if (0>n or n>4) {
+            s="Outside of range";
+            throw s;
+        }
+        else {
+            cout<<arr[n];
+        }
+    }
+    catch(string s) {
+        cout<<s<<endl;
+    }
+    return 0;
 }
